4b012ddf2d77c00449f0457a4f137e8e1cd1b6ea
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-16 02:54:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+  
+  def add_student(name,grade)
+   @roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each {|grade, name| @roster[grade] = name.sort}
+end
+end  
